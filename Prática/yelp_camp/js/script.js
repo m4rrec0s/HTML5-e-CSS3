819ab63f8172792a9ac30b35c7d51c345962968c
Marcos Henrique Araújo <mine.rique.games@gmail.com>
2023-12-11 21:25:07 -0300

    password

--- Prática/yelp_camp/js/script.js
+++ Prática/yelp_camp/js/script.js
@@ -6,6 +6,19 @@ document.querySelectorAll('.redirect-button').forEach(function(button) {
     });
 });
 
+document.getElementById('olho').addEventListener('mousedown', function() {
+    document.getElementById('password').type = 'text';
+  });
+  
+  document.getElementById('olho').addEventListener('mouseup', function() {
+    document.getElementById('password').type = 'password';
+  });
+  
+  // Para que o password não fique exposto apos mover a imagem.
+  document.getElementById('olho').addEventListener('mousemove', function() {
+    document.getElementById('password').type = 'password';
+});
+
 function createAccount() {
 
     const fs = require('fs');
