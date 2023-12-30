2d597320b9656dbf64593a48f6a074eed922f91f
Marcos Henrique Araújo <mine.rique.games@gmail.com>
2023-12-30 12:15:13 -0300

    ...

---
+++ portifolio/js/script.js
@@ -0,0 +1,8 @@
+window.sr = ScrollReveal({ reset : true });
+
+sr.reveal('.bloco1', {distance: '50px', duration: 1000});
+sr.reveal('.bloco2', {distance: '50px', duration: 1500});
+sr.reveal('.bloco3', {distance: '50px', duration: 1000});
+sr.reveal('.poj', {distance: '50px', interval: 10, reset: true, duration: 2000});
+sr.reveal('footer', {distance: '50px', duration: 1500});
+
